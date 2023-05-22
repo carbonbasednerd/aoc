6413561d15d778017aba53299f466534aece65c6
Jason Raffi <carbonbasednerd@gmail.com>
2023-05-22 19:52:36 -0400

    cleanup day 9 2016

--- 2016/day_9/solution_9.py
+++ 2016/day_9/solution_9.py
@@ -1,33 +1,3 @@
-def decompress(file):
-    decompression = 0
-    data = open(file, 'r')
-    for line in data:
-        stripped = line.strip()
-        counter = 0
-        marker = ""
-        reading_compression_marker = False
-        while counter < len(stripped):
-            if stripped[counter] == "(":
-                reading_compression_marker = True
-                counter += 1
-            elif stripped[counter] == ")" and reading_compression_marker:
-                reading_compression_marker = False
-                marker_split = marker.split("x")
-                decompression += (int(marker_split[0]) * int(marker_split[1]))
-                counter += int(marker_split[0]) + 1
-                marker = ""
-            else:
-                if reading_compression_marker:
-                    marker += stripped[counter]
-                    counter += 1
-                else:
-                    if stripped[counter] != " ":
-                        decompression += 1
-                    counter += 1
-
-    data.close()
-    return decompression
-
 
 def read_data(file):
     data = open(file, 'r')
@@ -36,9 +6,7 @@ def read_data(file):
     return output
 
 
-def decompress_nested(data):
-    # print(data)
-    decompression = 0
+def decompress_nested(data, allow_nesting):
     equation = ""
     marker = ""
     reading_compression_marker = False
@@ -52,7 +20,10 @@ def decompress_nested(data):
             elif c == ")":
                 reading_compression_marker = False
                 split = marker.split("x")
-                equation += f"{decompress_nested(data[count+1:count+int(split[0])+1]) * int(split[1])}+"
+                if allow_nesting:
+                    equation += f"{decompress_nested(data[count+1:count+int(split[0])+1], allow_nesting) * int(split[1])}+"
+                else:
+                    equation += f"{int(split[0])*int(split[1])}+"
                 skip = int(split[0])
                 marker = ""
 
@@ -66,13 +37,10 @@ def decompress_nested(data):
     if equation[-1] == "+":
         equation = equation[0:-1]
 
-    # print(f"equation:{equation}")
-
-    decompression = eval(equation)
-    return decompression
+    return eval(equation)
 
 
 if __name__ == "__main__":
-    print(f"size after decompression:{decompress('data_9')}")
+    print(f"size after decompression:{decompress_nested(read_data('data_9'), False)}")
 
-    print(f"size after nested decompression:{decompress_nested(read_data('data_9'))}")
+    print(f"size after nested decompression:{decompress_nested(read_data('data_9'), True)}")
