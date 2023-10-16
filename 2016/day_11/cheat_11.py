33e971a477e476edc7ab279b24e758f767f63462
Jason Raffi <carbonbasednerd@gmail.com>
2023-10-16 10:38:32 -0400

    with help, found a solution. need to cleanup and understand

---
+++ 2016/day_11/cheat_11.py
@@ -0,0 +1,71 @@
+import re
+from itertools import chain
+from itertools import combinations
+from collections import Counter
+from collections import deque
+
+
+def load_data(file):
+    data = open(file, 'r')   # cool. this very compactly grabs everything and sticks it in a set.
+    return [set(re.findall(r'(\w+)(?:-compatible)? (microchip|generator)', line))
+            for line in data]
+
+
+def is_valid_transition(floor):
+    return len(set(type for _, type in floor)) < 2 or \
+           all((obj, 'generator') in floor
+               for (obj, type) in floor
+               if type == 'microchip')
+
+
+def next_states(state):
+    moves, elevator, floors = state
+
+    possible_moves = chain(combinations(floors[elevator], 2), combinations(floors[elevator], 1))
+
+    for move in possible_moves:
+        for direction in [-1, 1]:
+            next_elevator = elevator + direction
+            if not 0 <= next_elevator < len(floors):
+                continue
+
+            next_floors = floors.copy()
+            next_floors[elevator] = next_floors[elevator].difference(move)
+            next_floors[next_elevator] = next_floors[next_elevator].union(move)
+
+            if (is_valid_transition(next_floors[elevator]) and is_valid_transition(next_floors[next_elevator])):
+                yield (moves + 1, next_elevator, next_floors)
+
+
+def count_floor_objects(state):
+    _, elevator, floors = state
+    return elevator, tuple(tuple(Counter(type for _, type in floor).most_common()) for floor in floors)
+
+
+def is_all_top_level(floors):
+    return all(not floor
+               for number, floor in enumerate(floors)
+               if number < len(floors) - 1)
+
+
+def min_moves_to_top_level(floors):
+    seen = set()
+    queue = deque([(0, 0, floors)])
+
+    while queue:
+        state = queue.popleft()
+        moves, _, floors = state
+
+        if is_all_top_level(floors):
+            return moves
+
+        for next_state in next_states(state):
+            if (key := count_floor_objects(next_state)) not in seen:
+                seen.add(key)
+                queue.append(next_state)
+
+
+if __name__ == "__main__":
+    initial_state = load_data("data_11")
+
+    print(min_moves_to_top_level(initial_state))
--- 2016/day_11/solution_11.py
+++ 2016/day_11/solution_11.py
@@ -1,5 +1,6 @@
 import itertools
 import copy
+from collections import Counter
 
 
 def load_data(file):
@@ -34,101 +35,132 @@ def generate_states(parent_state, old_states):
     elif parent_state[0] == 3:
         floors_to_check.append(2)
     else:
-        floors_to_check.append(parent_state[0] - 1)
         floors_to_check.append(parent_state[0] + 1)
+        floors_to_check.append(parent_state[0] - 1)
+    # elif parent_state[0] == 1:
+    #     floors_to_check.append(parent_state[0] + 1)
+    #     if parent_state[1][0]:
+    #         floors_to_check.append(parent_state[0] - 1)
+    # else:
+    #     floors_to_check.append(parent_state[0] + 1)
+    #     if parent_state[1][0] and parent_state[1][1]:
+    #         floors_to_check.append(parent_state[0] - 1)
 
     next_states = []
+    failed_states = []
     current_floor = parent_state[0]
     for floor in floors_to_check:
         combos = list(itertools.chain.from_iterable(itertools.combinations(parent_state[1][current_floor], i) for i in range(1, 3)))
         for components in combos:
             new_state = copy.deepcopy(parent_state)
             new_state[0] = floor
+            new_state[3] = copy.deepcopy(parent_state[1])
             for component in components:
                 new_state[1][floor].append(component)
                 new_state[1][floor].sort()
                 new_state[1][current_floor].remove(component)
 
             if check_for_goal(new_state):
-                return [], True
+                return [], True, []
 
-            if not check_for_prior_state(new_state, old_states) and check_if_safe_simplified(new_state[1][floor]):
+            if check_if_safe_simplified(new_state[1]):
+                # if check_distance(new_state[1]) > parent_state[2] - 1 and not check_priors(new_state, old_states):
+                # if not check_local_priors(new_state):
                 next_states.append(new_state)
+            else:
+                failed_states.append(new_state)
 
-    return next_states, False
+            # if not check_local_priors(new_state):
+            #     if check_if_safe_simplified(new_state[1]):
+            #         if check_for_goal(new_state):
+            #             return [], True, []
+            #
+            #         new_state[2] = check_distance(new_state[1])
+            #         if new_state[2] >= parent_state[2] - 1:
+            #             next_states.append(new_state)
+            #         else:
+            #             failed_states.append(new_state)
+            #     else:
+            #         failed_states.append(new_state)
+
+    return next_states, False, failed_states
+
+
+def check_distance(s):
+    return (1 * len(s[0])) + (2 * len(s[1])) + (3 * len(s[2])) + (4 * len(s[3]))
 
 
 def check_for_goal(potential_goal):
     return potential_goal[0] == 3 and len(potential_goal[1][0]) == 0 and len(potential_goal[1][1]) == 0 and len(potential_goal[1][2]) == 0
 
 
-def state_not_previously_done(new_state, old_states):
-    for old in old_states:
-        if old[1][0] == new_state[1][0] and old[1][1] == new_state[1][1] and old[1][2] == new_state[1][2] and old[1][3] == new_state[1][3]:
-            return False
-    return True
-
-
-def check_for_prior_state(new_state, old_states):
-    hashed = hash_a_state(new_state)
-    for old_state in old_states:
-        if hashed[0] == old_state[0] and hashed[1] == old_state[1] and hashed[2] == old_state[2] and hashed[3] == old_state[3]:
+def check_local_priors(new_state):
+    if new_state[3]:
+        if new_state[3][0] == new_state[1][0] and new_state[3][1] == new_state[1][1] and new_state[3][2] == new_state[1][2] and\
+                new_state[3][3] == new_state[1][3]:
             return True
     return False
 
 
-def hash_a_state(state_to_hash):
-    hashed_state = []
-    for i in range(0, 4):
-        flattened = list(itertools.chain.from_iterable(state_to_hash[1][i]))
-        stringed = ''.join(flattened).replace(" ", "")
-        hashed_state.append(hash(stringed))
-    return hashed_state
+def check_priors(new_state, old_states):
+    if old_states[new_state[0]]:
+        for old_state in old_states.get(new_state[0]):
+            if old_state[1][0] == new_state[1][0] and old_state[1][1] == new_state[1][1] and old_state[1][2] == new_state[1][2] and old_state[1][3] == new_state[1][3]:
+                return True
+    return False
 
 
-def check_if_safe_simplified(floor):
-    generators = []
-    chips = []
-    for component in floor:
-        if component[1] == "generator":
-            generators.append(component)
-        else:
-            chips.append(component)
+def check_if_safe_simplified(check_state):
+    for floor in check_state:
+        generators = []
+        chips = []
+        for component in floor:
+            if component[1] == "generator":
+                generators.append(component)
+            else:
+                chips.append(component)
 
-    if generators and chips:
-        for chip in chips:
-            chip_type = chip[0].split("-")[0]
-            if chip_type not in (map(lambda x: x[0], generators)):
-                return False
+        if generators and chips:
+            for chip in chips:
+                chip_type = chip[0].split("-")[0]
+                if chip_type not in (map(lambda x: x[0], generators)):
+                    return False
     return True
+
+
+def count_floor_objects(state):
+    return state[0], tuple(tuple(Counter(type for _, type in floor).most_common()) for floor in state[1])
 
 
 if __name__ == "__main__":
     initial_state = load_data("data_11")
     solving = True
-    prior_state = copy.deepcopy(initial_state)
-    states = [[0, initial_state]]
-    prior_states = []
+    states = [[0, initial_state, check_distance(initial_state), []]]
+    prior_states = {0: [], 1: [], 2: [], 3: []}
     moves = 0
+    seen = set()
+
     while solving:
         print(f"moves: {moves}")
         new_states = []
-        rejected_states = []
-        checked_states = []
         for state in states:
-            queued_states, solved = generate_states(state, prior_states)
+            queued_states, solved, failed = generate_states(state, prior_states)
             if solved:
                 solving = False
                 break
-            checked_states.append(hash_a_state(state))
-            new_states.extend(queued_states)
 
-        prior_states = copy.deepcopy(checked_states)
-        states = copy.deepcopy(new_states)
+            for queue in queued_states:
+                if (key := count_floor_objects(queue)) not in seen:
+                    seen.add(key)
+                    new_states.append(queue)
+
+            # prior_states[state[0]].append(state)
+            # for fail in failed:
+            #     prior_states[fail[0]].append(fail)
+            # new_states.extend(queued_states)
+
+        states = new_states
         moves += 1
         print(f"new states {len(new_states)}")
         if not solving:
             print(f"Found solution after {moves} moves")
-
-
-# todo: hash the prior states and keep them in a list for quicker look up. from itertools import chain, list(chain.from_iterable(y)), hash(), .join()
